f54501b3f2f2d31622d9c09bf95dbbc9a5000f96
ricardofreitas <ricawf@gmail.com>
2017-06-05 15:52:21 +0100

    ExprTestCodeGenerator

--- src/com/comp/code_generator/generators/ExprTestCodeGenerator.java
+++ src/com/comp/code_generator/generators/ExprTestCodeGenerator.java
@@ -1,6 +1,10 @@
 package com.comp.code_generator.generators;
 
 import vendor.Node;
+import com.comp.utils.services.NodeUtilsService;
+
+import static com.comp.semantic_analyser.NodeType.LHS;
+import static com.comp.semantic_analyser.NodeType.RHS;
 
 /**
  * @author Ricardo Wragg Freitas <ei95036@fe.up.pt> 199502870
@@ -9,6 +13,26 @@ public class ExprTestCodeGenerator extends CodeGenerator {
 
     @Override
     public String generate(Node node) {
-        return "";
+        Node
+            lhsNode = NodeUtilsService.getInstance().getChildOfType(node, LHS),
+            rhsNode = NodeUtilsService.getInstance().getChildOfType(node, RHS);
+
+        addLhs(node);
+        addRhs(node);
+
+        return code.toString();
     }
+
+    private void addLhs(Node node) {
+        String varId = node.getValue().toString();
+
+        if (node.hasChildren()) {
+
+        }
+    }
+
+    private void addRhs(Node node) {
+
+    }
+
 }
